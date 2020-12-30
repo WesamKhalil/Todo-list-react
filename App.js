df6f223b263b2b609bab826954c12e5ab8a2c68f
WesamKhalil <66225664+WesamKhalil@users.noreply.github.com>
2020-12-30 14:03:20 +0000

    Add files via upload
    Changed a bit of the styling. Put the form component on the item being edited instead of just returning the form component and not showing the rest of the list. Changed the placeholder text for the form when it's being rendered for editing an item, it now says 'Edit text "--text of item being edited here--"'. Also changed the text for the button on the form component to say 'submit edit' when being rendered for editing.

---
+++ App.js
@@ -0,0 +1,68 @@
+import React, { useState } from "react"
+import Form from "./Form.js"
+import ToDoList from "./ToDoList.js"
+import "./App.css"
+
+let App = props => {
+    const [todos, setTodos] = useState([])
+
+    const addTodos = todo => {
+        if(!todo.text || /^\s*$/.test(todo.text)) {
+            return
+        }
+
+        const newTodos = [todo, ...todos]
+
+        setTodos(newTodos)
+        console.log([todo, ...todos])
+    }
+
+    const removeTodo = id => {
+        const removeArr = [...todos].filter(todo => todo.id !== id)
+
+        setTodos(removeArr)
+    }
+
+    const editTodo = (id, obj) => {
+        console.log(obj)
+        if(!obj.text || /^\s*$/.test(obj.text)) {
+            return
+        }
+
+        setTodos(prev => prev.map(item => item.id === id ? obj : item))
+    }
+
+    const completeTodo = id => {
+        let updatedTodos = todos.map(todo => {
+            if(todo.id === id) {
+                todo.isComplete = !todo.isComplete
+            }
+            return todo
+        })
+        setTodos(updatedTodos)
+    }
+
+    return(
+        <div>
+            <div className="app-header">
+                <h1>Todays tasks.</h1>
+            </div>
+            <div className="app-cont">
+                <div className="form-cont">
+                    <Form 
+                        onSubmit={addTodos}
+                        placeholder={"Add a todo"}
+                        buttonText={"Add an item"}/>
+                </div>
+                <ToDoList 
+                    todos={todos}
+                    completeTodo={completeTodo}
+                    removeTodo={removeTodo}
+                    editTodo={editTodo}
+                />
+            </div>
+        </div>
+    )
+}
+
+export default App
